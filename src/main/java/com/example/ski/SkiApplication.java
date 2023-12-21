da89ee5e5219358eaff4883f5a68d5c3f57f5ad3
Omar Ben Fathallah <omarbenfathallah782@gmail.com>
2023-12-21 15:16:43 +0100

    Ajout des packages de base : entité, repository, service et controller

--- src/main/java/com/example/ski/SkiApplication.java
+++ src/main/java/com/example/ski/SkiApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SkiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SkiApplication.class, args);
 	}
 
 }
